84d1f06a3f5a1659083476d7f1d241a2966fb83b
Bernard-LTW <34589202+Bernard-LTW@users.noreply.github.com>
2022-11-09 15:34:47 +0900

    task002_edit2

--- Tasks/Task2_20221108.py
+++ Tasks/Task2_20221108.py
@@ -1,17 +1,19 @@
-#2022-11-08 Task 2
+# 2022-11-08 Task 2
 
-#Binary counter to LEDs on Arduinos
+# Binary counter to LEDs on Arduinos
+
+import time
 
 from pyfirmata import Arduino
-import time
 
 board = Arduino('/dev/cu.usbserial-120')
 print("Board is ready")
 
-for i in range(8):
-    board.digital[12].write(i//4)
-    time.sleep(1)
-    board.digital[5].write((i%4)//2)
-    time.sleep(1)
-    board.digital[9].write(i%2)
-    time.sleep(1)
+while True:
+    for i in range(8):
+        board.digital[12].write(i // 4)
+        time.sleep(1)
+        board.digital[5].write((i % 4) // 2)
+        time.sleep(1)
+        board.digital[9].write(i % 2)
+        time.sleep(1)
