48638bef9b99c4c6568653f697cfef47aba47364
Bernard-LTW <34589202+Bernard-LTW@users.noreply.github.com>
2022-11-25 10:10:53 +0900

    1121_done_lateupload

--- Lessons/2022-11-21_A.py
+++ Lessons/2022-11-21_A.py
@@ -1,8 +1,17 @@
 #2022-11-21 A
 
 import requests
+from matplotlib import pyplot as plt
 
 req = requests.get("http://192.168.6.142/readings")
 data = req.json()
 readings = data["readings"][0]
 print(len(readings))
+
+temp = []
+for sample in readings:
+    if sample["sensor_id"] == 2:
+        temp.append(sample["value"])
+
+plt.plot(temp)
+plt.show()
